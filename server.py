87f9f39f75442f93f102d204f353578db2dff8f7
gdy <gdy@exemple.com>
2018-01-12 17:00:54 +0100

    version de la mort...git add --all

--- server.py
+++ server.py
@@ -11,7 +11,7 @@ def index():
 
 @socketio.on('send_message')
 def handle_source(json_data):
-    //text = json_data['message'].encode('ascii', 'ignore')
+    ##text = json_data['message'].encode('ascii', 'ignore')
     socketio.emit('echo', {'rep':'Server Says...'})
 
 @socketio.on('connect')
